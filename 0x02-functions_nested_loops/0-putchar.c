700a246b5eedf5a1aed750fa94dcc845c4f795ef
jojomutanu003 <musalumutanu@gmail.com>
2022-07-05 04:41:30 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 /**
  *main-Entry point
  *Return:Always 0
 */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('e');
 	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
 	_putchar('\n');}
+
 	return (0);
 }
