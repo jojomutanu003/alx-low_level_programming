7efe1a9d6a86d0371f6d8fbefb51553e841d7ac0
jojomutanu003 <musalumutanu@gmail.com>
2022-07-14 01:07:55 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
  *leet-Encodes a string into 1337
- *@s-The string to encode
+ *@s:The string to encode
  *Return:The encoded string
 */
+
 char *leet(char *s)
 {
 	int a = 0, b = 0, l = 5;
 	char r[5] = {'A', 'E', 'O', 'T', 'L'};
 	char n[5] = {'4', '3', '0', '7', '1'};
-	
+
 	while (s[a])
 	{
 	b = 0;
